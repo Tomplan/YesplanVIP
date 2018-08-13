ba1458f7e7c779b2078ce7d488f3274ef1a16ae7
techcc <techcc-foh-video@ptr-luor29g2qyynscbdr0.18120a2.ip6.access.telenet.be>
2018-08-13 14:15:44 +0200

    APIFunction Branch created DictionaryExtension added

---
+++ YesplanVIP/DictionaryExtension.swift
@@ -0,0 +1,23 @@
+//
+//  DictionaryExtension.swift
+//  YesplanVIP
+//
+//  Created by Techcc - FOH - Video on 13/08/18.
+//  Copyright © 2018 Yesplan. All rights reserved.
+//
+
+import Foundation
+
+extension Dictionary {
+    mutating func merge<S: Sequence>(contentsOf other: S) where S.Iterator.Element == (key: Key, value: Value) {
+        for (key, value) in other {
+            self[key] = value
+        }
+    }
+    
+    func merged<S: Sequence>(with other: S) -> [Key: Value] where S.Iterator.Element == (key: Key, value: Value) {
+        var dic = self
+        dic.merge(contentsOf: other)
+        return dic
+    }
+}
