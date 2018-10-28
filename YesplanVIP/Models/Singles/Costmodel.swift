71ea97c64bf1b7fa30ded1b4f9b858de7b820b45
techcc <techcc-foh-video@techccs-macbook-pro-4.local>
2018-10-28 15:34:49 +0100

    lost in translation II

---
+++ YesplanVIP/Models/Singles/Costmodel.swift
@@ -0,0 +1,25 @@
+//
+//  Costmodel.swift
+//  YesplanVIP
+//
+//  Created by Techcc - FOH - Video on 7/10/18.
+//  Copyright © 2018 Yesplan. All rights reserved.
+//
+
+import Foundation
+import Arrow
+
+struct Costmodel: Codable {
+    let name: String?
+    let cost: String //  check
+    let costformula: Formula
+//    let price: String
+//    let priceformula: Formula
+//    let account: String?
+//    let invoice: Bool
+//    let durationdiscount: DiscountFormula
+//    let volumediscount: DiscountFormula
+//    let purchaseprice: Double
+//    let vat: Double
+//    let profiles: [Profile]
+}
