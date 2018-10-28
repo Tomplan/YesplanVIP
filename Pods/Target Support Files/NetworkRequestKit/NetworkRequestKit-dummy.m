48622c3af021fb4d577dbc5d2c4c9c89bdd05d14
techcc <techcc-foh-video@techccs-macbook-pro-4.local>
2018-10-28 15:36:59 +0100

    lost in translation V

--- Pods/Target Support Files/NetworkRequestKit/NetworkRequestKit-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_NetworkRequestKit : NSObject
-@end
-@implementation PodsDummy_NetworkRequestKit
-@end
--- Pods/Target Support Files/Pods-YesplanVIPTests/Pods-YesplanVIPTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_YesplanVIPTests : NSObject
-@end
-@implementation PodsDummy_Pods_YesplanVIPTests
-@end
