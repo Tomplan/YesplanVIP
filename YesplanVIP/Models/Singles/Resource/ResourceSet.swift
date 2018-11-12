66c749340f6fbfb18f0093a354f1ad6f6cfdbea6
techcc <techcc-foh-video@techccs-macbook-pro-4.local>
2018-11-12 15:29:28 +0100

    gedeeltelijk: EDTeam

--- YesplanVIP/Models/Singles/Resource/ResourceSet.swift
+++ YesplanVIP/Models/Singles/Resource/ResourceSet.swift
@@ -11,45 +11,23 @@ import Foundation
 
 struct ResourceSet: Codable {
     var url: URL = URL(string: "http://")!
-    var id: String = ""
+    var id: String
 //    var resourcetype: String?
-    var name: String = ""
+    var name: String
 //    var group: String?
 //    var roles: [String]?
 //    var description: String?
 //    var external: Bool = false
 //    var rented: Bool = false
 //    var costmodels: [Costmodel]?
-    var type: String = ""
+    var type: String
 //    var owner: User?
 //    var owningteam: Usergroup?
 //    var owninggroup: Usergroup?
-    var _type: String = ""
+    var _type: String
 //    var amount: Int?
 //    var resources: [Base] = []
     
-    init() { guard self._type == "resource" else {
-        return } }
+//    init() { guard self._type == "resource" else {
+//        return } }
 }
-//
-//extension ResourceSet: ArrowParsable {
-//    mutating func deserialize(_ json: JSON) {
-//        url <-- json["url"]
-//        id <-- json["id"]
-//        resourcetype <-- json["resourcetype"]
-//        name <-- json["name"]
-//        group <-- json["group"]
-//        roles <-- json["roles"]
-//        description <-- json["description"]
-//        external <-- json["external"]
-//        rented <-- json["rented"]
-//        costmodels <-- json["costmodels"]
-//        amount <-- json["amount"]
-//        type <-- json["type"]
-//        owner <-- json["owner"]
-//        owningteam <-- json["owningteam"]
-//        owninggroup <-- json["owninggroup"]
-//        _type <-- json["_type"]
-//        resources <-- json["resources"]
-//    }
-//}
